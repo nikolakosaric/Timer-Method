b09d9986a6a605b1ee1ba095fb72dcae0416c8e3
Nikola Kosaric <nikolakosaric@gmail.com>
2019-02-04 22:15:25 -0500

    Updated for use with MSP430F5529

--- main.c
+++ main.c
@@ -1,23 +1,20 @@
-#include <msp430.h>
+#include <msp430.h> 
 
 int main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	
+	P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
+	P1DIR |= BIT0;                          // Set P1.0 to output direction
 
-    P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
-    P1DIR |= BIT0;                          // Set P1.0 to output direction
+	TA0CTL = TASSEL_1 | ID_2 | MC_2;
+	TA0CCR0 = 16000;
 
-    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
-                                            // to activate previously configured port settings
-
-    TB0CTL = TBSSEL_1 | ID_2 | MC_2;
-    TB0CCR0 = 16000;
-
-    while(1) {
-        if(TB0R > TB0CCR0){
-            P1OUT |= BIT0;
-        } else{
-            P1OUT &= ~BIT0;
-        }
-    }
+	while(1) {
+	    if(TA0R > TA0CCR0){
+	        P1OUT |= BIT0;
+	    } else{
+	        P1OUT &= ~BIT0;
+	    }
+	}
 }
